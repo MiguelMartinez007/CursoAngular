6f9e13a653aa524905d7aac130ae8e289099e8f1
MIKE <MiguelMartinezDW@outlook.com>
2018-06-06 19:22:20 -0500

    Finalizamos con la practica de rutas con parametros

--- Angular/02-spa/src/app/components/heroes/heroes.component.ts
+++ Angular/02-spa/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Route, Router } from "@angular/router";
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +10,18 @@ export class HeroesComponent implements OnInit {
 
   heroes:Heroe [] = [];
 
-  constructor( private _heroesService:HeroesService ) {
-    console.log("Constructor");
+  constructor( private _heroesService:HeroesService, private _router:Router ) {
+    // console.log("Constructor");
    }
 
   ngOnInit() { // Es utilizado cuando la pagina ya esta renderizada
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes);        
+    // console.log(this.heroes);        
   }
+
+  verHeroe (idx:number){
+    // console.log(idx);
+    this._router.navigate( ['/heroe', idx] )
+  };
 
 }
