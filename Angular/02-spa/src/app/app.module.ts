c18ffd4285943db498d08f1eeeb95752a4460431
MIKE <MiguelMartinezDW@outlook.com>
2018-06-08 20:06:47 -0500

    Programa guardado hasta l momento
    Seguimos con la tarea

--- Angular/02-spa/src/app/app.module.ts
+++ Angular/02-spa/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HeroeTargetaComponent } from './components/heroe-targeta/heroe-targeta.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { BuscadorComponent } from './components/buscador/buscador.component';
     AboutComponent,
     HeroesComponent,
     HeroeComponent,
-    BuscadorComponent
+    BuscadorComponent,
+    HeroeTargetaComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/02-spa/src/app/components/heroe-targeta/heroe-targeta.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Route, Router } from "@angular/router";
+
+@Component({
+  selector: 'app-heroe-targeta',
+  templateUrl: './heroe-targeta.component.html',
+  styleUrls: ['./heroe-targeta.component.css']
+})
+export class HeroeTargetaComponent implements OnInit {
+
+  @Input() heroe:any = {};
+  @Input() index:number;
+  
+
+  constructor( private router:Router ) { }
+
+  ngOnInit() {
+  }
+
+
+  verHeroe(){
+    console.log( this.index );
+    this.router.navigate( ['/heroe', this.index] )
+  }
+
+}
