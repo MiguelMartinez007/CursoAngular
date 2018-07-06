f517f39df4d9f5a153cfed2f75f0fd01a3bead19
MIKE <MiguelMartinezDW@outlook.com>
2018-07-06 16:02:34 -0500

    se creo ejemplo de peticion get

--- Angular/04-SpotiApp/src/app/app.module.ts
+++ Angular/04-SpotiApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
@@ -22,6 +22,7 @@ import { ROUTES } from "./app.routes";
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot( ROUTES, { useHash: true } )
   ],
   providers: [],
