a03a41aca278007d82699454c8001e5c2e8b7ab5
MIKE <MiguelMartinezDW@outlook.com>
2018-07-06 11:56:24 -0500

    Se crearon las rutas

--- Angular/04-SpotiApp/src/app/app.module.ts
+++ Angular/04-SpotiApp/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from "@angular/router";
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { SearchComponent } from './component/search/search.component';
 import { ArtistaComponent } from './component/artista/artista.component';
 import { NavbarComponent } from './component/shared/navbar/navbar.component';
+
+// Importar rutas
+import { ROUTES } from "./app.routes";
 
 @NgModule({
   declarations: [
@@ -16,7 +21,8 @@ import { NavbarComponent } from './component/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( ROUTES, { useHash: true } )
   ],
   providers: [],
   bootstrap: [AppComponent]
